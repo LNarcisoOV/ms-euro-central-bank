a1318fcf5020cfbe4ad52b8e0c08ad8fad4ffc53
Leonardo Narciso <LNarcisoOV@gmail.com>
2022-05-19 15:37:52 +0000

    First commit.

---
+++ src/main/java/com/ecb/MsEuroCentralBankApplication.java
@@ -0,0 +1,13 @@
+package com.ecb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEuroCentralBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEuroCentralBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecb/MsEuroCentralBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEuroCentralBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
