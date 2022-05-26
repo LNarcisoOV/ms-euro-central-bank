9ba899b89a3ef17d6e4a5d0c06b8b7d833cb12c3
Leonardo Narciso <LNarcisoOV@gmail.com>
2022-05-26 12:20:23 +0000

    Add response entity to currency controller.

--- src/main/java/com/ecb/controller/CurrencyController.java
+++ src/main/java/com/ecb/controller/CurrencyController.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,32 +24,39 @@ public class CurrencyController {
 	private CurrencyService currencyService;
 
 	@GetMapping("/{startDate}")
-	public List<Currency> getByDate(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate) {
-		return currencyService.getByDate(startDate);
+	public ResponseEntity<List<Currency>> getByDate(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate) {
+		return new ResponseEntity<>(currencyService.getByDate(startDate), HttpStatus.OK);
 	}
 
 	@GetMapping("/{startDate}/{originCurrency}/{destinationCurrency}/{value}")
-	public BigDecimal getConvertedValueByParameter(
+	public ResponseEntity<BigDecimal> getConvertedValueByParameter(
 			@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
 			@PathVariable String originCurrency, @PathVariable String destinationCurrency,
 			@PathVariable BigDecimal value) {
-		return currencyService.getConvertedValueBy(startDate, originCurrency, destinationCurrency, value);
+		try {
+			BigDecimal convertedValue = currencyService.getConvertedValueBy(startDate, originCurrency, destinationCurrency, value);
+			return new ResponseEntity<>(convertedValue, HttpStatus.OK);
+		} catch (RuntimeException runtimeException) {
+			throw runtimeException;
+		}
 	}
 	
 	@GetMapping("/{startDate}/{endDate}/{originCurrency}")
-	public BigDecimal getHigherValueByParameters(
+	public ResponseEntity<BigDecimal> getHigherValueByParameters(
 			@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
 			@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate,
 			@PathVariable String originCurrency) {
-		return currencyService.getHigherValueBy(startDate, endDate, originCurrency);
+		BigDecimal higherValue = currencyService.getHigherValueBy(startDate, endDate, originCurrency);
+		return new ResponseEntity<>(higherValue, HttpStatus.OK);
 	}
 	
 	@GetMapping("/avarage/{startDate}/{endDate}/{originCurrency}")
-	public BigDecimal getAvarageValue(
+	public ResponseEntity<BigDecimal>  getAvarageValue(
 			@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
 			@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate,
 			@PathVariable String originCurrency) {
-		return currencyService.getAvarageValueBy(startDate, endDate, originCurrency);
+		BigDecimal avarageValue = currencyService.getAvarageValueBy(startDate, endDate, originCurrency);
+		return new ResponseEntity<>(avarageValue, HttpStatus.OK);
 	}
 
 }
